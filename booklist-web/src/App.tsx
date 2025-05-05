e0a9282e52299d3d0f75d965807d8fd682dab6a4
DevHredoy <rafiul.hredoy@technonext.com>
2025-05-05 17:59:35 +0600

    tailwind configured. the change is still not there.

--- booklist-web/src/App.tsx
+++ booklist-web/src/App.tsx
@@ -1,10 +1,12 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-   return (
-    <div>hello my name is hredoy . I wanted to rise in the sky. That was a big dream</div>
-  )
+  return (
+    <div className="text-3xl font-serif text-red-700">
+      hello my name is hredoy . I wanted to rise in the sky. That was a big
+      dream
+    </div>
+  );
 }
 
-export default App
+export default App;
